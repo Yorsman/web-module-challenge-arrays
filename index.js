3649f4e2771ab3c3bbdfea789361d64cf119f96b
Yorsman Guatemala <yorsguatemala@gmail.com>
2020-06-28 16:14:53 -0400

    finished is31Flavors function

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ var originalFlavors = ["Banana Nut Fudge",
     "Vanilla",
     "Vanilla Burnt Almond"]
 
+
 /* Task 1: Confirm that the array is exactly 31 flavors. Your function should accept:
 
 (1) an array 
@@ -40,12 +41,15 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
-}
-
+function is31Flavors(originalFlavors){
+    const total = originalFlavors.length;
+    if(total == 31){
+        return true;
+    }else{
+        return false;
+    };
+};
+is31Flavors(originalFlavors);
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
 Your function should accept:
@@ -57,7 +61,7 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(flavor, array){
 
     /*code here*/
 
